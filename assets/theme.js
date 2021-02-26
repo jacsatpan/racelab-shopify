b3e5bfaf33ad679090be5420759ea5b038b1ae9d
Jacs <jacs@pan.co.nz>
2021-02-26 15:02:01 +1300

    quick fix: add page reload for variant price bug

--- assets/theme.js
+++ assets/theme.js
@@ -5191,4 +5191,15 @@ theme.init = function() {
 $(theme.init);
 
 
+// custom 
+
+jQuery(function () {
+  $('.single-option-selector').on('change', function () {
+    setTimeout(function () {
+      if ($('[name="id"]').val() != "{{ product.selected_or_first_available_variant.id }}") {
+        location.reload();
+      }
+    }, 1);
+  });
+});
 
